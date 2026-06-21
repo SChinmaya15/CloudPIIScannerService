e09c4b95e1347f901bd57103b780cf0f9ae13bf8
Bhavneet Kumar <bhavneet.kumar@itq.in>
2026-06-21 13:27:46 +0530

    Alignment changes

--- CloudPiiScannerService/Models/AgentRegistrationRequest.cs
+++ CloudPiiScannerService/Models/AgentRegistrationRequest.cs
@@ -1,12 +1,17 @@
 namespace CloudPiiScannerService.Models
 {
- public class AgentRegistrationRequest
- {
- public string MachineName { get; set; } = default!;
- public string CurrentUser { get; set; } = default!;
- public string MacAddress { get; set; } = default!;
- public string OperatingSystem { get; set; } = default!;
- public string OsVersion { get; set; } = default!;
- public string AgentVersion { get; set; } = default!;
- }
+    public class AgentRegistrationRequest
+    {
+        public string OsVersion { get; set; } = default!;
+        
+        public string MacAddress { get; set; } = default!;
+        
+        public string MachineName { get; set; } = default!;
+        
+        public string CurrentUser { get; set; } = default!;
+        
+        public string AgentVersion { get; set; } = default!;
+        
+        public string OperatingSystem { get; set; } = default!;
+    }
 }
